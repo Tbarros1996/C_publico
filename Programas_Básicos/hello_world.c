1e9c3c8fcb123794bdea5c2fa55eb85ba2b80faf
Thiago Barros <86922706+Tbarros1996@users.noreply.github.com>
2021-10-20 11:19:47 -0300

    Add files via upload

---
+++ Programas_Básicos/hello_world.c
@@ -0,0 +1,24 @@
+//Computação Científica Para Engenharia
+//Programa Desenvolvido por Thiago Barros
+
+
+/*
+
+Programa 1: Hello World
+
+*/
+
+//Inclusão das Bibliotecas Padrão
+
+#include<stdio.h>
+#include<stdlib.h>
+
+//Definição da Função Principal
+
+ int main(){ 
+
+    printf("Olá Mundo!!");
+    system("pause");
+    return 0;
+
+}
